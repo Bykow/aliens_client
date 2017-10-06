4c3e912afebee825956d4fe77ee791ea42bfc8d9
Lawrence <bykow12345@gmail.com>
2017-10-06 16:43:26 +0200

    c'est joli

--- docs/assets/js/aliens.js
+++ docs/assets/js/aliens.js
@@ -1,18 +1,23 @@
-const data = require('../../data/hireableUsers.json');
 
-function createTable(jsonData) {
-  for (let i = 0; i < jsonData.length; i++) {
-    $('#table-aliens').find('tbody').append(`\
-        <tr>\
-           <td>${jsonData[i].id}</td>\
-           <td>${jsonData[i].avatar_url}</td>\
-           <td>${jsonData[i].name}</td>\
-           <td>${jsonData[i].login}</td>\
-           <td>${jsonData[i].location}</td>\
-        </tr>\
+function createTable() {
+  $.getJSON('data/hireableUsers.json', (jsonData) => {
+    for (let i = 0; i < jsonData.length; i++) {
+      $('#table-aliens').find('tbody').append(`
+        <tr>
+           <td>${jsonData[i].id}</td>
+           <td><img src='${jsonData[i].avatar_url}' width="50" height="50" /></td>
+           <td>${jsonData[i].name}</td>
+           <td>
+            <a href='${jsonData[i].html_url}'>
+                ${jsonData[i].login}
+            </a>
+           </td>
+           <td>${jsonData[i].location}</td>
+        </tr>
         `);
-  }
+    }
+  });
 }
 
-createTable(JSON.parse(data));
+createTable();
 
