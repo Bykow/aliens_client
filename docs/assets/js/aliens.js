b04851d729e9eb6ccfb1e549795bbf16c899dbcf
Lawrence <bykow12345@gmail.com>
2017-10-09 13:45:17 +0200

    Cleaned and centered

--- docs/assets/js/aliens.js
+++ docs/assets/js/aliens.js
@@ -1,18 +1,25 @@
-
 function createTable() {
   $.getJSON('data/hireableUsers.json', (jsonData) => {
-    for (let i = 0; i < jsonData.length; i++) {
+    $('#header-aliens').append(`
+          <p class="category"> They have more than ${jsonData.info.followers} followers
+           and more than ${jsonData.info.repo} repos on GitHub !</p>
+        `);
+
+    // Sorting the devs by number of followers
+    jsonData.values.sort((a, b) => parseFloat(b.followers) - parseFloat(a.followers));
+
+    for (let i = 0; i < jsonData.values.length; i++) {
       $('#table-aliens').find('tbody').append(`
         <tr>
-           <td>${jsonData[i].id}</td>
-           <td><img src='${jsonData[i].avatar_url}' width="50" height="50" /></td>
-           <td>${jsonData[i].name}</td>
+           <td><img src='${jsonData.values[i].avatar_url}' width="50" height="50" /></td>
+           <td>${jsonData.values[i].name}</td>
            <td>
-            <a href='${jsonData[i].html_url}'>
-                ${jsonData[i].login}
+            <a href='${jsonData.values[i].html_url}' target="_blank">
+                ${jsonData.values[i].login}
             </a>
            </td>
-           <td>${jsonData[i].location}</td>
+           <td>${jsonData.values[i].location}</td>
+           <td>${jsonData.values[i].followers}</td>
         </tr>
         `);
     }
